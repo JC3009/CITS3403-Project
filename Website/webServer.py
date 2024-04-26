95f5cc7bf890abc4ba41f5db1381ebc25dcac66a
Tyler Etherton <=>
2024-04-26 13:50:35 +0800

    Added shell context to webserver file - this allows for easy access to the database in the shell for debugging

--- Website/webServer.py
+++ Website/webServer.py
@@ -1,1 +1,8 @@
-from app import flaskApp
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import flaskApp, db
+from app.models import User, TradieUser
+
+@flaskApp.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'TradieUser': TradieUser}
