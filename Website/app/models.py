93171eda99bb9fd793d15c28fff4a302c182a0b1
Tyler Etherton <=>
2024-04-26 14:08:04 +0800

    Added login functionality - changed login view function to authenticate users via database - added set and check password methods to User model

--- Website/app/models.py
+++ Website/app/models.py
@@ -1,18 +1,30 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    tradie: so.Mapped['TradieUser'] = so.relationship(back_populates='user')
+    tradie: so.Mapped['TradieUser'] = so.relationship('TradieUser', back_populates='user')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"name: {self.username} contact: {self.email}" #str conversion
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 #to be updated for db integration
 class TradieUser(db.Model):
@@ -23,7 +35,7 @@ class TradieUser(db.Model):
     certified: so.Mapped[bool] = so.mapped_column(sa.Boolean, default=0)#functionality which allows the user to be considered certified through external process if they provide their valid trade license
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True) #The user account associated with this tradie account
 
-    user: so.Mapped[User] = so.relationship(back_populates='tradie')
+    user: so.Mapped[User] = so.relationship('User', back_populates='tradie')
 
     def __repr__(self):
         return f"user: {self.user_id} does {self.trade}" #str conversion
--- Website/app/routes.py
+++ Website/app/routes.py
@@ -1,12 +1,25 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import flaskApp
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @flaskApp.route('/')
 def home():
     return render_template('page_main.html')
 
-@flaskApp.route('/login')
+@flaskApp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('home'))
     return render_template('login.html', form=form)
