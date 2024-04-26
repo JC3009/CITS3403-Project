70239f51a2a89cbae927fcd19d41d57ee8c529fd
Tyler Etherton <=>
2024-04-26 17:23:16 +0800

    Added registration page - link in sign up now points to register page - register.html template created - registrationform created in forms.py - registration route created in routes.py - registration correctly adds user account to database and allows for logging in.
    Problems
    - register link is positioned weirdly on login page
    - email input extends outside the boundary on the registration page
    - will create issues for these 2 problems.

--- Website/app/routes.py
+++ Website/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash
 from app import flaskApp
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app import db
@@ -28,3 +28,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@flaskApp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now registered!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
