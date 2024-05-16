a4dd3cb1bbb5b418b1b382722442f44082811b97
Tyler Etherton <=>
2024-05-16 10:13:14 +0800

    Moved image model to models.py created models for location, job_request, job_offer created migration for new models

--- Website/app/forms.py
+++ Website/app/forms.py
@@ -33,8 +33,4 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(
             User.email == email.data))
         if user is not None:
-            raise ValidationError('Email adress already in use.')
-        
-class Image(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.LargeBinary)
+              raise ValidationError('Email adress already in use.')
